45bdfd2d1b3b2f2098cdcf876958d213b3ba8d7f
ads692 <ads692@gmail.com>
2016-08-17 22:30:25 -0700

    Config file

---
+++ PHP Server/config.php
@@ -0,0 +1,8 @@
+<?php
+
+// see README.md for an explanation of these config params
+$TWILIO_ACCOUNT_SID = 'AC8c783b8aa94ed16b747291f3b900d765';
+$TWILIO_CONFIGURATION_SID = 'VS964f5916bc1b7dc13d6076128939b5e9';
+$TWILIO_API_KEY = 'SKeba0a52ee7451bd02aaa243ac13c94d1';
+$TWILIO_API_SECRET = 'c6QRBNWQwrgNm2zhYuepVIy0An2XCqPT';
+
